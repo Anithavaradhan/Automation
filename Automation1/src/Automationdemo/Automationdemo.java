41cc45685574769e4bcfd1e453441a3552b1fe4d
Anitha <anithaa357@gmail.com>
2020-02-26 21:58:13 +1300

    Created a class file if else condition

---
+++ Automation1/src/Automationdemo/Automationdemo.java
@@ -0,0 +1,23 @@
+package Automationdemo;
+
+public class Automationdemo {
+
+	public static void main(String[] args) {
+		int a=20;
+		int b=30;
+		if(a>b)
+		{
+		System.out.println("a is biggest");
+		}
+		else
+		{
+		System.out.println("b is biggest");
+		}
+
+		}
+
+				
+
+	}
+
+
